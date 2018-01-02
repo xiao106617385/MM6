4e2c11c6b4ba5da531de856e8c2d4f6e1e09fc9f
xiao106617385 <you@example.com>
2018-01-02 12:06:28 +0800

    随便写写

---
+++ MM6/Assets/Scripts/Model/BaseItem.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ *  物品基类
+ */ 
+public class BaseItem
+{
+
+}
---
+++ MM6/Assets/Scripts/Model/BaseUnit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+ *  单位基类
+ *  单位固有属性
+ */ 
+
+public class BaseUnit
+{
+    public int ID;
+    public string Name;
+    public int MaxHP;
+    public int MaxMP;
+    public int HP;
+    public int MP;
+
+
+    public Vector3 MyPosition;
+    public List<BaseItem> MyGoods;
+    public List<BaseUnit> MyEnemyList;
+    public List<BaseUnit> MyFriendList;
+}
